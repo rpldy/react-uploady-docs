25b28db00e41154fcb4dd3ece64e13e728f9461d
yoavniran <yoavniran@gmail.com>
2022-01-23 00:40:40 +0200

    getting started wip copied guides npm2yarn plugin

---
+++ docs/getting-started/installation.md
@@ -0,0 +1,22 @@
+---
+id: installation
+title: Installation
+description: How to install react-uploady to get started
+sidebar_position: 0
+---
+
+# Installation
+
+## Requirements
+
+React-Uploady defines react & react-dom as peer dependencies with the minimum version of: __16.8.0__
+
+
+
+```bash npm2yarn
+npm install @rpldy/uploady
+```
+
+```bash npm2yarn
+npm run bla
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,18 +3,19 @@
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const npm2yarn = require("@docusaurus/remark-plugin-npm2yarn");
 
 /** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: "React-Uploady",
-  tagline: "Modern file-upload components & hooks for React.",
-  url: "https://react-uploady.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "https://res.cloudinary.com/yoav-cloud/image/upload/w_192,r_max/v1585756811/rpldy/logo/logo-white-bg.png",
-  organizationName: "rpldy",
-  projectName: "react-uploady",
+	title: "React-Uploady",
+	tagline: "Modern file-upload components & hooks for React.",
+	url: "https://react-uploady.com",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "https://res.cloudinary.com/yoav-cloud/image/upload/w_192,r_max/v1585756811/rpldy/logo/logo-white-bg.png",
+	organizationName: "rpldy",
+	projectName: "react-uploady",
 
 	// metadata: [{name: 'twitter:card', content: 'summary'}],
 
@@ -23,132 +24,151 @@ const config = {
 	// 		'An optimized site generator in React. Docusaurus helps you to move fast and write content. Build documentation websites, blogs, marketing pages, and more.',
 	// },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import("@docusaurus/preset-classic").Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+	presets: [
+		[
+			"classic",
+			/** @type {import("@docusaurus/preset-classic").Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
 
-	        showLastUpdateAuthor: true,
-	        showLastUpdateTime: true,
+					// showLastUpdateAuthor: true,
+					showLastUpdateTime: true,
 
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
-    ],
-  ],
+					remarkPlugins: [[npm2yarn, {sync: true}]],
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
-    ({
-	    image: "img/uploady/react-uploady-text-logo.png",
+	themeConfig:
+	/** @type {import("@docusaurus/preset-classic").ThemeConfig} */
+		({
+			image: "img/uploady/react-uploady-text-logo.png",
 
-	    announcementBar: {
-		    id: 'rpldy-announce-1', // Increment on change
-		    content: `⭐️ If you like Uploady, please give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/rpldy/react-uploady">GitHub</a> ⭐`,
-	    },
+			announcementBar: {
+				id: "rpldy-announce-1", // Increment on change
+				content: `⭐️ If you like Uploady, please give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/rpldy/react-uploady">GitHub</a> ⭐`,
+			},
 
-      navbar: {
-	      hideOnScroll: true,
-        title: "React-Uploady",
-        logo: {
-          alt: "Uploady Logo",
-          src: "img/uploady/react-uploady.png",
-	        srcDark: "img/uploady/react-uploady-light.png",
-        },
-        items: [
-	        //left-side
-          {
-            type: "doc",
-            docId: "introduction",
-            position: "left",
-            label: "Docs",
-          },
-	        {
-		        type: "doc",
-		        docId: "api/api",
-		        position: "left",
-		        label: "API",
-	        },
-          {to: "/blog", label: "Blog", position: "left"},
+			navbar: {
+				hideOnScroll: true,
+				title: "React-Uploady",
+				logo: {
+					alt: "Uploady Logo",
+					src: "img/uploady/react-uploady.png",
+					srcDark: "img/uploady/react-uploady-light.png",
+				},
+				items: [
+					//left-side
+					{
+						type: "doc",
+						docId: "introduction",
+						position: "left",
+						label: "Docs",
+					},
+					{
+						type: "doc",
+						docId: "api/api",
+						position: "left",
+						label: "API",
+					},
+					{ to: "/blog", label: "Blog", position: "left" },
 
 
-	        //right-side
-	        {
-		        href: "https://github.com/rpldy/react-uploady",
-		        position: "right",
-		        className: "header-github-link",
-		        "aria-label": "GitHub repository",
-          },
+					//right-side
+					{
+						href: "https://github.com/rpldy/react-uploady",
+						position: "right",
+						className: "header-github-link",
+						"aria-label": "GitHub repository",
+					},
 
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Docs",
-                to: "/docs/intro",
-              },
-	            {
-		            label: "API",
-		            to: "/docs/api",
-	            },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} React-Uploady, Yoav Niran.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [
+					{
+						title: "Docs",
+						items: [
+							{
+								label: "Docs",
+								to: "/docs/intro",
+							},
+							{
+								label: "API",
+								to: "/docs/api",
+							},
+						],
+					},
+					{
+						title: "Community",
+						items: [
+							{
+								label: "Stack Overflow",
+								href: "https://stackoverflow.com/questions/tagged/docusaurus",
+							},
+							{
+								label: "Discord",
+								href: "https://discordapp.com/invite/docusaurus",
+							},
+							{
+								label: "Twitter",
+								href: "https://twitter.com/poeticGeek",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "Blog",
+								to: "/blog",
+							},
+							{
+								label: "GitHub",
+								href: "https://github.com/rpldy/react-uploady",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} React-Uploady, Yoav Niran.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
+
+	plugins: [
+		[
+			"@docusaurus/plugin-sitemap",
+			{
+				changefreq: "weekly",
+				priority: 0.5,
+			},
+		],
+
+		//TODO - configure PWA plugin!!
+		// [
+		// 	"@docusaurus/plugin-pwa",
+		// 	{
+		//
+		// 	}
+		// ]
+	],
 };
 
 module.exports = config;
